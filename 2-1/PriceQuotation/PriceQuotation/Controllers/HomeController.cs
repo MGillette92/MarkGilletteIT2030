a119c6b769b7dab5db4d8f5fbddfa3483355c133
MGillette92 <36048864+MGillette92@users.noreply.github.com>
2021-09-15 17:53:49 -0400

    added 2-1

---
+++ 2-1/PriceQuotation/PriceQuotation/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotation.Models;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Message = 0;
+            ViewBag.Message2 = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+
+                ViewBag.Message = model.CalculateDiscountAmount();
+                ViewBag.Message2 = model.CalculatePriceQuotation(); ;
+
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = 0;
+                ViewBag.Message2 = 0;
+            }
+            return View(model);
+        }
+    }
+}
